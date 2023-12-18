b475ffcd5854e55a63e0f24b3a8e238477736183
Sasi <hey@sasi.codes>
2023-12-18 21:55:52 +0530

    chore: add basePath to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const withMDX = require('@next/mdx')()
 const nextConfig = {
+  assetPrefix: 'https://open-actions-directory.vercel.app',
   // Configure `pageExtensions`` to include MDX files
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   // Optionally, add any other Next.js config below
